aaecd60ba0652525aaa635e59dbc0b2e2fdc6624
mh-tagizadeh <mohammadhusseinbvb09@gmail.com>
2021-02-17 19:02:06 +0330

    create admin frontend ui

---
+++ resources/views/admin/app.blade.php
@@ -0,0 +1,23 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <title>@yield('title') - {{ config('app.name') }}</title>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <link rel="stylesheet" type="text/css" href="{{ asset('backend/css/main.css') }}" />
+    <link rel="stylesheet" type="text/css" href="{{ asset('backend/css/font-awesome/4.7.0/css/font-awesome.min.css') }}"/>
+</head>
+<body class="app sidebar-mini rtl">
+    @include('admin.partials.header')
+    @include('admin.partials.sidebar')
+    <main class="app-content">
+        @yield('content')
+    </main>
+    <script src="{{ asset('backend/js/jquery-3.2.1.min.js') }}"></script>
+    <script src="{{ asset('backend/js/popper.min.js') }}"></script>
+    <script src="{{ asset('backend/js/bootstrap.min.js') }}"></script>
+    <script src="{{ asset('backend/js/main.js') }}"></script>
+    <script src="{{ asset('backend/js/plugins/pace.min.js') }}"></script> 
+</body>
+</html>
--- webpack.mix.js
+++ webpack.mix.js
@@ -11,7 +11,11 @@ const mix = require('laravel-mix');
  |
  */
 
-mix.js('resources/js/app.js', 'public/js')
-    .postCss('resources/css/app.css', 'public/css', [
-        //
-    ]);
+// mix.js('resources/js/app.js', 'public/js')
+//     .postCss('resources/css/app.css', 'public/css', [
+//         //
+//     ]);
+
+
+mix.copyDirectory('resources/backend', 'public/backend');
+mix.copyDirectory('resources/frontend', 'public/frontend');
