fda10f242e7b2989a5539d1551e8c0f4838f9e34
mh-tagizadeh <mohammadhusseinbvb09@gmail.com>
2021-02-24 12:44:51 +0330

    createing attribute repository and binding attribute contract to repository

--- app/Providers/RepositoryServiceProvider.php
+++ app/Providers/RepositoryServiceProvider.php
@@ -5,12 +5,15 @@ namespace App\Providers;
 use Illuminate\Support\ServiceProvider;
 use App\Contracts\CategoryContract;
 use App\Repositories\CategoryRepository;
+use App\Contracts\AttributeContract;
+use App\Repositories\AttributeRepository;
 
 
 class RepositoryServiceProvider extends ServiceProvider
 {
     protected $repositories = [
-        CategoryContract::class => CategoryRepository::class,
+        CategoryContract::class     =>  CategoryRepository::class,
+        AttributeContract::class    =>  AttributeRepository::class,
     ];
     /**
      * Register services.
