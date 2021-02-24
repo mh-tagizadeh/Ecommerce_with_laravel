be754faea4996069936cc78900f1fc3bfa8abe5d
mh-tagizadeh <mohammadhusseinbvb09@gmail.com>
2021-02-24 12:58:54 +0330

    Creating Attributes Create Page

--- app/Http/Controllers/Admin/AttributeController.php
+++ app/Http/Controllers/Admin/AttributeController.php
@@ -37,7 +37,8 @@ class AttributeController extends BaseController
      */
     public function create()
     {
-        //
+        $this->setPageTitle('Attributes', 'Create Attribute');
+        return view('admin.attributes.create');
     }
 
     /**
@@ -48,7 +49,20 @@ class AttributeController extends BaseController
      */
     public function store(Request $request)
     {
-        //
+        $this->validate($request, [
+            'code'          =>  'required',
+            'name'          =>  'required',
+            'frontend_type' =>  'required'
+        ]);
+
+        $params = $request->except('_token');
+
+        $attribute = $this->attributeRepository->createAttribute($params);
+
+        if (!$attribute) {
+            return $this->responseRedirectBack('Error occurred while creating attribute.', 'error', true, true);
+        }
+        return $this->responseRedirect('admin.attributes.index', 'Attribute added successfully' ,'success',false, false);
     }
 
     /**
