d02248eb877e2c50e7e96a9b7ecddf1aeefbea38
mh-tagizadeh <mohammadhusseinbvb09@gmail.com>
2021-02-24 12:55:23 +0330

    Creating Attribures List Page

---
+++ app/Http/Controllers/Admin/AttributeController.php
@@ -0,0 +1,98 @@
+<?php
+
+namespace App\Http\Controllers\Admin;
+
+use App\Http\Controllers\BaseController;
+use App\Models\Attribute;
+use Illuminate\Http\Request;
+use App\Contracts\AttributeContract;
+
+class AttributeController extends BaseController
+{
+
+    protected $attributeRepository;
+
+    public function __construct(AttributeContract $attributeRepository)
+    {
+        $this->attributeRepository = $attributeRepository;
+    }
+
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index()
+    {
+        $attributes = $this->attributeRepository->listAttributes();
+
+        $this->setPageTitle('Attributes', 'List of all attributes');
+        return view('admin.attributes.index', compact('attributes'));
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function create()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function store(Request $request)
+    {
+        //
+    }
+
+    /**
+     * Display the specified resource.
+     *
+     * @param  \App\Models\Attribute  $attribute
+     * @return \Illuminate\Http\Response
+     */
+    public function show(Attribute $attribute)
+    {
+        //
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     *
+     * @param  \App\Models\Attribute  $attribute
+     * @return \Illuminate\Http\Response
+     */
+    public function edit(Attribute $attribute)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  \App\Models\Attribute  $attribute
+     * @return \Illuminate\Http\Response
+     */
+    public function update(Request $request, Attribute $attribute)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  \App\Models\Attribute  $attribute
+     * @return \Illuminate\Http\Response
+     */
+    public function destroy(Attribute $attribute)
+    {
+        //
+    }
+}
