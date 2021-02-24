75eebdb527e20fe97b3291771022e0b8e20bca94
mh-tagizadeh <mohammadhusseinbvb09@gmail.com>
2021-02-24 11:47:49 +0330

    create attribute value seed class

---
+++ app/Models/AttributeValue.php
@@ -0,0 +1,31 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class AttributeValue extends Model
+{
+    use HasFactory;
+
+    /**
+     * 
+     * @var string
+     */
+    protected $table = 'attribute_values';
+
+    /**
+     * @var array
+     */
+    protected $fillable = [
+        'attribute_id', 'value', 'price'
+    ];
+
+    /**
+     * @var array
+     */
+    protected $casts = [
+        'attribute_id' => 'integer',
+    ];
+}
